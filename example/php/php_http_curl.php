b2d74fba95fa1d06eb0cd5b2fb2eac8fd0cd8c7d
lestercx <126766269+lestercx@users.noreply.github.com>
2023-08-14 22:58:53 +0800

    Update php_http_curl.php

--- example/php/php_http_curl.php
+++ example/php/php_http_curl.php
@@ -2,7 +2,7 @@
 
 $params = '{"trace":"c5e6d492-baed-40bf-9b27-93119d2d4a0d-16920159919524472","data":{"data_list":[{"code":"5.HK","kline_type":"8","kline_timestamp_end":"0","query_kline_num":"1","adjust_type":"0"},{"code":"9988.HK","kline_type":"8","kline_timestamp_end":"0","query_kline_num":"1","adjust_type":"0"},{"code":"3690.HK","kline_type":"8","kline_timestamp_end":"0","query_kline_num":"1","adjust_type":"0"},{"code":"700.HK","kline_type":"8","kline_timestamp_end":"0","query_kline_num":"1","adjust_type":"0"},{"code":"2601.HK","kline_type":"8","kline_timestamp_end":"0","query_kline_num":"1","adjust_type":"0"}]}}';
 
-$url = 'https://quote.aatest.online/quote-stock-b-api/batch-kline?token=e945d7d9-9e6e-4721-922a-7251a9d311d0-1678159756806';
+$url = 'https://quote.tradeswitcher.com/quote-stock-b-api/batch-kline?token=e945d7d9-9e6e-4721-922a-7251a9d311d0-1678159756806';
 $method = 'GET';
 
 
